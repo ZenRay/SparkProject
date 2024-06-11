af6168453ce0aae71f3949df6d9897e92cb0668d
ZenRay <jake2011ren@gmail.com>
2024-06-12 03:00:46 +0800

    Update File Create RDD and add log configuration

--- src/main/scala/com/example/rdd/FileCreated.scala
+++ src/main/scala/com/example/rdd/FileCreated.scala
@@ -1,5 +1,6 @@
 package com.example.rdd
 
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 object FileCreated {
@@ -7,12 +8,18 @@ object FileCreated {
         // 1. 创建连接
         val conf = new SparkConf()
             .setMaster("local[*]")
-            .setMaster("File Created RDD")
+            .setAppName("File Created RDD")
 
         val sparkContext = new SparkContext(conf)
 
         // 2. 读取文件数据
-        sparkContext.textFile()
+        // 读取文件的时候，可以使用正则通配符的方式，使用 * 替代多个字符；当然也可以读取 HDFS 文件目录
+        val rdd: RDD[String] = sparkContext.textFile("./data/input/rddFile.txt")
+
+        rdd.collect().foreach(println)
+
+        // 3. 关闭连接
+        sparkContext.stop()
     }
 
 }
